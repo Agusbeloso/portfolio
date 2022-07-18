f4a0ba514cbd66c704c410c09d35611ba62e8a63
Agusbeloso <belosoan@gmail.com>
2022-07-18 02:04:21 -0300

    cambios

--- menu.php
+++ menu.php
@@ -1,22 +1,21 @@
         <nav class="navbar navbar-expand-md mb-4">
             <div class="container-fluid">
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
-                    aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarCollapse">
                     <ul class="navbar-nav me-auto mb-2 mb-md-0">
                         <li class="nav-item">
-                            <a class="nav-link px-4 py-1" href="index.php">Inicio</a>
+                            <a class="nav-link px-4 py-1" <?php echo ($pg == "inicio") ? "active" : ""; ?>" href="index.php">Inicio</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link px-4 py-1" href="sobre-mi.php">Sobre mí</a>
+                            <a class="nav-link px-4 py-1" <?php echo ($pg == "sobre-mi") ? "active" : ""; ?>" href="sobre-mi.php">Sobre mí</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link px-4 py-1 active" href="proyectos.php">Proyectos</a>
+                            <a class="nav-link px-4 py-1" <?php echo ($pg == "proyectos") ? "active" : ""; ?>" href="proyectos.php">Proyectos</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link px-4 py-1" href="contacto.php">Contacto</a>
+                            <a class="nav-link px-4 py-1" <?php echo ($pg == "contacto") ? "active" : ""; ?>" href="contacto.php" href="contacto.php">Contacto</a>
                         </li>
                     </ul>
                     <div class="d-inline">
@@ -25,4 +24,3 @@
                 </div>
             </div>
         </nav>
-   
